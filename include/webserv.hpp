289b9db985da782c76303a6353be8a0716528f61
Anska <103422661+algadea@users.noreply.github.com>
2025-06-17 19:51:47 +0200

    testBranch: init

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -6,12 +6,24 @@
 /*   By: algadea <algadea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 20:10:14 by algadea           #+#    #+#             */
-/*   Updated: 2025/06/16 12:32:35 by algadea          ###   ########.fr       */
+/*   Updated: 2025/06/17 19:39:06 by algadea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEBSERV_HPP
 #define WEBSERV_HPP
 
+#include <poll.h>
+#include <netdb.h>
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
 
 #endif
