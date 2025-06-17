032d129f82e870090e29da94fc6ba6cc5a71c3c0
Anska <103422661+algadea@users.noreply.github.com>
2025-06-18 00:43:18 +0200

    update files

--- include/WebServ.hpp
+++ include/WebServ.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*   WebServ.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: algadea <algadea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 20:10:14 by algadea           #+#    #+#             */
-/*   Updated: 2025/06/17 19:39:06 by algadea          ###   ########.fr       */
+/*   Updated: 2025/06/17 23:17:56 by algadea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,16 @@
 #include <sys/types.h>
 #include <sys/select.h>
 #include <sys/socket.h>
+
+typedef struct s_config_file {
+    char    *name;
+}t_config_file;
+
+class WebServ {
+public:
+
+private:
+
+};
 
 #endif
