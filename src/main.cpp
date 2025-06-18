e11d889a078aa0d04ec986f95c95ce2f1592437f
Anska <103422661+algadea@users.noreply.github.com>
2025-06-18 22:28:23 +0200

    update commit

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,6 @@ int setsockopt(int sockfd, int level, int optname,
                     const void *optval, socklen_t optlen);
 int getsockname(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 struct protoent *getprotobyname(const char *name);
-int fcntl(int fd, int cmd, ... / arg / );
 int close(int fd);
 ssize_t read(int fd, void *buf, size_t count);
 ssize_t write(int fd, const void *buf, size_t count);
