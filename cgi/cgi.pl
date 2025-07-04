c229c0046a3d83a56067d5ea035d4287ce6c833f
Anska <103422661+algadea@users.noreply.github.com>
2025-07-04 19:24:36 +0200

    cgi: update cgi scripts

---
+++ cgi/cgi.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+print "Content-type: text/html\n\n";
+print <<'END_HTML';
+<!DOCTYPE html>
+<html>
+<head><title>Perl CGI Script</title></head>
+<body>
+<h1>Hello from Perl CGI!</h1>
+<p>This is a CGI script written in Perl.</p>
+</body>
+</html>
+END_HTML
