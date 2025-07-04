e8a93fd5232c5e9a150fff3f8f22fa9744189294
Anska <103422661+algadea@users.noreply.github.com>
2025-07-04 19:24:36 +0200

    cgi: update cgi scripts

---
+++ cgi/cgi.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php-cgi
+<?php
+echo "Content-type: text/html\n\n";
+?>
+<!DOCTYPE html>
+<html>
+<head><title>PHP CGI Script</title></head>
+<body>
+<h1>Hello from PHP CGI!</h1>
+<p>This is a CGI script written in PHP.</p>
+</body>
+</html>
---
+++ cgi/cgi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print("Content-type: text/html\n")
+print("""
+<!DOCTYPE html>
+<html>
+<head><title>Python CGI Script</title></head>
+<body>
+<h1>Hello from Python CGI!</h1>
+<p>This is a CGI script written in Python.</p>
+</body>
+</html>
+""")
