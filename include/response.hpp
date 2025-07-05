48b1999954d7e66fa87e0c2449467a1080764459
Kai-lin Yang <kyang@f6r3s14.clusters.42paris.fr>
2025-07-05 17:57:02 +0200

    request

---
+++ include/response.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cerrno>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+
+#include "../include/httpRequest.hpp"
+
+class response {
+
+	public:
+        response(httpRequest &request);
+        void setCode(int code);
+        void setHeaders(std::string &key, std::string &value);
+        void setBody(std::string &body);
+    
+    private:
+        void    build();
+        std::string statusLine;
+        std::map<std::string, std::string> headers;
+        std::string body;
+
+};
