dfd6b8f46bab89e85330bae314f61bad54b491d8
Anska <103422661+algadea@users.noreply.github.com>
2025-07-05 17:56:11 +0200

    update src and include files

--- src/multiplexer/epoll.cpp
+++ src/multiplexer/epoll.cpp
@@ -2,8 +2,6 @@
 
 void    run_using_epoll(ServerSocket& server) {
     server.bindAndListen();
-    std::cout << BOLD WHITE << "Server listening on port: "
-    << BOLD BLUE << "8080\n"
-    << BOLD WHITE  << "Multiplexer: " << BOLD BLUE << "epoll" << std::endl;
+    server.printServerStatus("epoll");
     server.acceptClientEpoll();
 }
--- src/multiplexer/select.cpp
+++ src/multiplexer/select.cpp
@@ -2,8 +2,6 @@
 
 void    run_using_select(ServerSocket& server) {
     server.bindAndListen();
-    std::cout << BOLD WHITE << "Server listening on port: "
-    << BOLD BLUE << "8080\n"
-    << BOLD WHITE  << "Multiplexer: " << BOLD BLUE << "select" << std::endl;
+    server.printServerStatus("select");
     server.acceptClientSelect();
 }
