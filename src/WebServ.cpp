7ae938bea8720a4e1b235169f14786ee9b4ffd51
Alexandre Lacroix <alacroix@f4r10s13.clusters.42paris.fr>
2025-07-09 15:19:49 +0200

    creating epoll wip

--- src/WebServ.cpp
+++ src/WebServ.cpp
@@ -33,8 +33,8 @@ WebServ::WebServ(const char* configFile) : _configFile(configFile), _portServie(
 WebServ::~WebServ() {
     if (this->_serverFd)
         close(this->_serverFd);
+    if(this->_epollFd);
+        close(this->_epollFd);
     for (fdsIterator it = this->_clientFds.begin(); it != this->_clientFds.end(); ++it)
         close(*it);
 }
-
-
