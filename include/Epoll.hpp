09748b5176fcf0a6de3f6fb77acf5c9409c364e1
Alexandre Lacroix <alacroix@f6r10s4.clusters.42paris.fr>
2025-07-10 12:23:47 +0200

    divided epoll::run into multiple functions with simple behaviour

--- include/Epoll.hpp
+++ include/Epoll.hpp
@@ -2,20 +2,27 @@
 
 #include "Socket.hpp"
 #include <sys/epoll.h>
+#include <vector>
+
+#define MAXEVENTS 4096
 
 class WebServ;
 
 class Epoll : public Socket {
 public:
-    void acceptClient();
+    void createEpollInstance();
+    int acceptClient();
+    void addServerToEpool();
+    void addClientToEpool(int const &clientFd);
     void run(WebServ& server);
     const int& getEpollFd(void) const;
+
+    typedef struct epoll_event epoll_ev;
+    typedef std::vector<epoll_ev> vector;
 
     Epoll();
     ~Epoll();
 
 private:
     int _epollFd;
-    struct epoll_event server_ev;
-
 };
