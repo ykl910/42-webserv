1e5666238d7381d329005ecf970df6607d0e9556
Kai-lin Yang <kyang@f6r5s6.clusters.42paris.fr>
2025-07-16 16:51:18 +0200

    non blocking select working

--- include/Select.hpp
+++ include/Select.hpp
@@ -9,13 +9,22 @@ class WebServ;
 
 class Select : public Socket {
 public:
+    bool acceptClient(int serverFd);
+    void readAndWrite(void);
     void run(WebServ<Select>& server);
-
+    
     Select();
     ~Select();
 
 private:
-    typedef std::vector<int>::iterator fdsIterator;
-    std::vector<int> _clientFds;
-
+    typedef std::vector<int>::iterator selectIterator;
+    fd_set              _readFds;
+    std::vector<int>    _selectFd;
 };
+
+/*
+  fd_set
+       A structure type that can represent a set of file descriptors.   According  to  POSIX,
+       the  maximum  number  of  file  descriptors in an fd_set structure is the value of the
+       macro FD_SETSIZE.
+*/
