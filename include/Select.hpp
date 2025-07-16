8abc2c32a16d85e6e15323322a2f65bc22fc007b
Alexandre Lacroix <alacroix@f6r5s4.clusters.42paris.fr>
2025-07-16 11:53:57 +0200

    recovered old select.cpp/.hpp because the new one is not functionnal yet

--- include/Select.hpp
+++ include/Select.hpp
@@ -9,30 +9,13 @@ class WebServ;
 
 class Select : public Socket {
 public:
-    void initSelect(void);
-    void checkReadFds(void);
-    void checkWriteFds(void);
-    void checkExceptFds(void);
     void run(WebServ<Select>& server);
-
-    int acceptClient(void);
 
     Select();
     ~Select();
 
 private:
-    typedef std::vector<int>::iterator selectIterator;
+    typedef std::vector<int>::iterator fdsIterator;
+    std::vector<int> _clientFds;
 
-    char                _buffer[4096];
-    fd_set              _readFds;
-    fd_set              _writeFds;
-    fd_set              _exceptFds;
-    std::vector<int>    _selectFd;
 };
-
-/*
-  fd_set
-       A structure type that can represent a set of file descriptors.   According  to  POSIX,
-       the  maximum  number  of  file  descriptors in an fd_set structure is the value of the
-       macro FD_SETSIZE.
-*/
--- src/Select.cpp
+++ src/Select.cpp
@@ -1,114 +1,80 @@
 #include "../include/Select.hpp"
 #include "../include/WebServ.hpp"
 
-int Select::acceptClient(void) {
-    return 0;
-}
-
-void    Select::initSelect(void) {
-    return;
-}
-
-void    Select::checkReadFds() {
-    return;
-}
-
-void    Select::checkWriteFds() {
-    return;
-}
-
-void    Select::checkExceptFds() {
-    return;
-}
-
 void    Select::run(WebServ<Select>& server) {
     server.printServerStatus("select");
-
+    fd_set readFds;
     struct timeval tv;
     tv.tv_sec = 10;
     tv.tv_usec = 0;
     int maxFd = this->getSocketFd();
     int serverFd = this->getSocketFd();
-    int activity = 0;
 
     while (true) {
+        //* FD_ZERO = empty readFds set
+        //* FD_SET = add server socket to detect new connexions
+        FD_ZERO(&readFds);
+        FD_SET(serverFd, &readFds);
 
-        FD_ZERO(&_readFds);
-        /*
-            This  macro  clears  (removes all file descriptors from) set.
-            It should be employed as the first step in initializing a file
-            descriptor set.
-        */
-
-        FD_SET(serverFd, &_readFds);
-        /*
-            This macro adds the file descriptor fd to set.  Adding a file
-            descriptor  that is already present in the set is a no-op, and does
-            not produce an error.
-        */
-
-        //* Add every client sockets to readFds and add maxFd if necessary
-        for (selectIterator it = this->_selectFd.begin();
-                            it != this->_selectFd.end(); ++it) {
-            FD_SET(*it, &_readFds);
+        //* Add everyt client sockets to readFds and add maxFd if necessary
+        for (fdsIterator it = this->_clientFds.begin();
+            it != this->_clientFds.end(); ++it) {
+            FD_SET(*it, &readFds);
             if (*it > maxFd)
                 maxFd = *it;
         }
 
+        tv.tv_sec = 10;
+        tv.tv_usec = 0;
+
         //* wait for event in a socket
         errno = 0;
-        activity = select(maxFd + 1, &_readFds, &_writeFds, &_exceptFds, &tv);
+        int activity = select(maxFd + 1, &readFds, NULL, NULL, &tv);
         if (activity < 0) {
-            printError(); // select error
+            printError();
             continue;
-        } else if (activity == 0) {
-            printError(); // timeout
         }
 
         //* new connexion -> accept connexion and add client to the list
-        if (FD_ISSET(serverFd, &_readFds)) {
-        /*
-            select() modifies the contents of the sets according to the rules
-            described below. After  calling  select(),  the FD_ISSET() macro
-            can be used to test if a file descriptor is still present in a set.
-            FD_ISSET() returns nonzero  if  the file descriptor fd is present
-            in set, and zero if it is not.
-        */
+        if (FD_ISSET(serverFd, &readFds)) {
             errno = 0;
             int newClient = accept(serverFd, NULL, NULL);
             if (newClient < 0) {
                 printError();
                 continue;
             } else {
-                this->_selectFd.push_back(newClient);
+                this->_clientFds.push_back(newClient);
                 std::cout << "New client connected: FD " << newClient << std::endl;
             }
         }
 
         //* loop on every actives clients and seek for data to read
-        for (selectIterator it = this->_selectFd.begin();
-                            it != this->_selectFd.end(); it++) {
+        for (fdsIterator it = this->_clientFds.begin(); it != this->_clientFds.end();) {
+            char buf[4096];
             int bytes = 0;
 
-            if (FD_ISSET(*it, &_readFds)) {
-                bytes = recv(*it, this->_buffer, sizeof(_buffer), 0);
+            if (FD_ISSET(*it, &readFds)) {
+                bytes = recv(*it, buf, sizeof(buf), 0);
                 if (bytes <= 0) {
                     close(*it);
                     std::cout << "Client disconnected: FD " << *it << std::endl;
-                    it = this->_selectFd.erase(it);
+                    it = this->_clientFds.erase(it);
                     continue;
                 }
 
-                std::string request(_buffer, bytes);
+                std::string request(buf, bytes);
                 HttpRequest httpReq(request);
                 std::cout << "Received request:\n" << request << std::endl;
                 std::cout << "Path:\n" << httpReq.getPath() << std::endl;
                 HttpResponse httpRes(httpReq);
                 httpRes.build(httpReq);
                 std::string response = httpRes.getResponse();
+                //std::cout << "Sending response:\n" << response << std::endl;
+                //std::cout << response << std::endl;
+                //std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello from webserv!</h1></body></html>";
                 send(*it, response.c_str(), response.size(), 0);
                 close(*it);
-                it = this->_selectFd.erase(it);
+                it = this->_clientFds.erase(it);
             }
             else
                 ++it;
@@ -116,10 +82,12 @@ void    Select::run(WebServ<Select>& server) {
     }
 }
 
-Select::Select() {}
+Select::Select() {
+
+}
 
 Select::~Select() {
-    for (selectIterator it = this->_selectFd.begin();
-         it != this->_selectFd.end(); ++it)
+    for (fdsIterator it = this->_clientFds.begin(); it != this->_clientFds.end(); ++it)
         close(*it);
+
 }
