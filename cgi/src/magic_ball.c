31075d7aa7e328fdc51e8a13c23ae5e10907d6d6
Alexandre Lacroix <alacroix@f4r11s3.clusters.42paris.fr>
2025-07-23 13:00:51 +0200

    adding new minigame + 8ball gif

---
+++ cgi/src/magic_ball.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * It is certain
+ * It is decidedly so
+ * Without a doubt
+ * You may rely on it
+ * As i see it, yes
+ * Most likely
+ * OUtlook good
+ * Yes
+ * Signs point to yes
+ * Reply hazy, try again
+ * Ask again later
+ * Better not tell you now
+ * Cannot predict now
+ * Concentrate and ask again
+ * Don't count on it
+ * My reply is no
+ * My sources say no
+ * Outlook not so good
+ * Very doubtful
+ */
+// 19 responses
+
+
+int main()
+{
+
+}
