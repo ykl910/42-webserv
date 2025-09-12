3bcd7ea09415b524745a2dcde10d6a00a45f055e
Kai-lin Yang <kyang@f6r6s12.clusters.42paris.fr>
2025-09-12 19:26:22 +0200

    fix threads error

--- www/post42.net/cgi/bin/threads.py
+++ www/post42.net/cgi/bin/threads.py
@@ -4,10 +4,13 @@ import os
 # Get the list of all files in a directory
 path = './www/post42.net/threads'
 files = os.listdir(path)
-res = "["
-for file in files:
-    res += '"'
-    res += file
-    res += '",'
-res = res[:-1] + ']'
+if files:
+    res = "["
+    for file in files:
+        res += '"'
+        res += file
+        res += '",'
+    res = res[:-1] + ']'
+else:
+    res = ""
 print(res)
