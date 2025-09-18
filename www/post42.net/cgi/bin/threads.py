74a7867987951b241a5fecd94fcb15204961f0a7
Kai-lin Yang <b00756817@essec.edu>
2025-09-18 11:43:42 +0200

    fix python

--- www/post42.net/cgi/bin/threads.py
+++ www/post42.net/cgi/bin/threads.py
@@ -3,14 +3,15 @@ import os
 
 # Get the list of all files in a directory
 path = './www/post42.net/downloads'
-files = os.listdir(path)
-if files:
-    res = "["
-    for file in files:
-        res += '"'
-        res += file
-        res += '",'
-    res = res[:-1] + ']'
+if os.path.exists(path):
+    files = os.listdir(path)
+    if files:
+        res = "["
+        for file in files:
+            res += '"' + file + '",'
+        res = res[:-1] + ']'
+    else:
+        res = ""
 else:
     res = ""
 print(res)
